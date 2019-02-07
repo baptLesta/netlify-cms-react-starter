94cacdf2ecb70a699fcbd46e2166fcd534e901ec
lestavelbaptiste <baptiste.lestavel@hei.fr>
2019-02-07 17:09:24 +0100

    Correct import issue for deploy.

--- cms/cms.js
+++ cms/cms.js
@@ -1,49 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-import '../src/globalStyles.css'
-import data from '../src/data.json'
-import Home from '../src/views/Home'
-import About from '../src/views/About'
-import Contact from '../src/views/Contact'
-import Blog from '../src/views/Blog'
-import SinglePost from '../src/views/SinglePost'
+import '../src/globalStyles.css';
+import data from '../src/data.json';
 
-console.log('React version', React.version)
+console.log('React version', React.version);
 
-const CMS = window.CMS
+const CMS = window.CMS;
 CMS.registerPreviewStyle(
   'https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css'
-)
-CMS.registerPreviewStyle('/admin/cms.bundle.css')
+);
+CMS.registerPreviewStyle('/admin/cms.bundle.css');
 
 const getDocument = (collection, name) =>
-  data[collection] && data[collection].filter(page => page.name === name)[0]
-const getDocuments = (collection, name) => data[collection]
+  data[collection] && data[collection].filter(page => page.name === name)[0];
+const getDocuments = (collection, name) => data[collection];
 
-const globalSettings = getDocument('settings', 'global')
-const posts = getDocuments('posts')
-
-// Preview Templates
-CMS.registerPreviewTemplate('home-page', ({ entry }) => (
-  <Home fields={entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('about-page', ({ entry }) => (
-  <About fields={entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('contact-page', ({ entry }) => (
-  <Contact fields={entry.toJS().data} siteTitle={globalSettings.siteTitle} />
-))
-CMS.registerPreviewTemplate('blog-page', ({ entry }) => (
-  <Blog fields={entry.toJS().data} posts={posts} />
-))
-CMS.registerPreviewTemplate('posts', ({ entry }) => (
-  <SinglePost fields={entry.toJS().data} />
-))
+const globalSettings = getDocument('settings', 'global');
+const posts = getDocuments('posts');
 
 // Return to home when user logging out
-window.netlifyIdentity.on('logout', function () {
-  document.location.href = '/'
-})
+window.netlifyIdentity.on('logout', function() {
+  document.location.href = '/';
+});
 
 // Log netlifySiteURL if editing on localhost
 if (
@@ -53,5 +31,5 @@ if (
   console.log(
     `%cnetlifySiteURL: ${window.localStorage.getItem('netlifySiteURL')}`,
     'color: hotpink; font-size: 15px'
-  )
+  );
 }
